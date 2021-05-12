6678c0d6a53a6aee9b5a3167d38ea9bc17b83e14
Aleksandar Milojevik <aleksandarmilojevik@gmail.com>
2021-05-12 13:18:24 +0200

    finished

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+const quiz = [
+    {
+        q:'What was Khal Drogo ?',
+        options: ['Lannister', 'Dothraki', 'Stark', 'Tyrell'],
+        answer:1
+    },
+    {
+        q:'What is Daenerys Targaryen`s nickname ?',
+        options: ['Sweety', 'Blondie', 'Dragon', 'Danny'],
+        answer:3
+    },
+    {
+        q:'Who killed the Night King ?',
+        options: ['Daenarys Targaryen', 'Arya Stark', 'John Snow', 'Tyrion Lannister'],
+        answer:1
+    },
+    {
+        q:'The name of Cersei`s brother and lover ?',
+        options: ['Jamie', 'Walden', 'Ned', 'Oberin'],
+        answer:0
+    },
+    {
+        q:'What is the name of the biggest dragon ?',
+        options: ['Bob', 'Black', 'Fire', 'Drogon'],
+        answer:3
+    },
+    {
+        q:'Who killed Prince Oberin ?',
+        options: ['The Mountain', 'Joffrey', 'Tywin', 'Eddard'],
+        answer:0
+    },
+    {
+        q:'What was the nickname of Petyr Baelish ?',
+        options: ['Mountain', 'Stormborn', 'LittleFinger', 'Demon'],
+        answer:2
+    },
+    {
+        q:'What was John Snow`s real name ?',
+        options: ['Peter', 'Ragnar', 'Aegon', 'Joffrey'],
+        answer:2
+    },
+    {
+        q:'What was holding the Free Folk from going south ?',
+        options: ['Lake', 'Wall', 'River', 'Monster'],
+        answer:1
+    },
+    {
+        q:'Ser Brienne was from ?',
+        options: ['Kings Landing', 'London', 'the North', 'tarth'],
+        answer:3
+    }
+]
